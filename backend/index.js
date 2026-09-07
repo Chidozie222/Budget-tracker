bdc915b2dbca805909d2b165358f3b40c243382a
Chidozie222 <ibennejohn064@gmail.com>
2026-09-06 21:28:17 -0700

    set up test environment

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,19 @@
 import express from "express";
+import { configDotenv } from "dotenv";
+configDotenv();
 // import cors from "cors";
+
+const PORT = process.env.PORT;
 
 const app = express();
 
 // app.use(cors());
 app.use(express.json());
 
-app.get("health", (req, res) => {
+app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok" });
+});
+
+app.listen(PORT, () => {
+  console.log(`This server is running on port ${PORT}`);
 });
