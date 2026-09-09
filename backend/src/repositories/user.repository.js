c257df153a5f748a2378e2c1f5066b82375f09de
Chidozie222 <ibennejohn064@gmail.com>
2026-09-09 10:55:01 -0700

    make the sign-up return the inputed data

--- backend/src/repositories/user.repository.js
+++ backend/src/repositories/user.repository.js
@@ -3,7 +3,7 @@ import { db } from "../config/db.js";
 export const post_user = async (name, email, password) => {
   const result = await db.query(
     `
-        INSERT INTO users (name, email, password, created_at, updated_at) VALUES ($1, $2, $3, NOW(), NOW())
+        INSERT INTO users (name, email, password, created_at, updated_at) VALUES ($1, $2, $3, NOW(), NOW()) RETURNING name, email, created_at, updated_at
         `,
     [name, email, password],
   );
